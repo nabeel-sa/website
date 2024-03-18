50a8c4540ab929394b55c5e4943935e693219d1e
nabeel-sa <sarhan.na@outlook.com>
2024-03-19 00:35:09 +0300

    Improve code compatibility

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,13 @@ const RootLayout = ({children}) => (
     <head>
         <link rel="preconnect" href="https://stijndv.com"/>
         <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css"/>
+        <meta name="author" content="Nabeel Sarhan"/>
+        <meta name="description" content="Nabeel's website and blog for web development and sharing knowledge"/>
+        <meta name="keywords"
+              content="personal website, Nabeel, Nabeel Sarhan, web development, sharing knowledge, excel, templates, free templates"/>
+        <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
+        <meta name="robots" content="index, follow"/>
+        <meta charSet="UTF-8"/>
     </head>
     <body>{children}</body>
     </html>
