dee3deebd5f86585553499cf5026990d39caab28
nabeel-sa <sarhan.na@outlook.com>
2024-03-18 20:23:24 +0300

    Improve code compatibility

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   experimental: {
-    appDir: true,
-  },
-};
+    appDir: true
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
