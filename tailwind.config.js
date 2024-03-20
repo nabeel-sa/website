92edbadce0423298ded0c7b64d2e2e4887f5d25f
nabeel-sa <sarhan.na@outlook.com>
2024-03-20 12:28:16 +0300

    Adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = { content: [ './app/**/*.{html,js,jsx}', './components/**/*.{html,js,jsx}', './sections/**/*.{html,js,jsx}', './styles/**/*.{js,jsx}', ],
+module.exports = { content: [ './app/**/*.{html,js,jsx}', './pages/blog/**/*.{html,js,jsx}', './components/**/*.{html,js,jsx}', './sections/**/*.{html,js,jsx}', './styles/**/*.{js,jsx}', ],
   mode: 'jit', theme: { extend: {
     colors: {
-      'primary-black': '#000000',
-      'secondary-white': '#F1F5F8',
+      'primary-black': '#1A1B1C',
+      'secondary-white': '#F1E1D1',
       }, transitionTimingFunction: { 'out-flex': 'cubic-bezier(0.05, 0.6, 0.4, 0.9)', }, }, }, plugins: [], };
